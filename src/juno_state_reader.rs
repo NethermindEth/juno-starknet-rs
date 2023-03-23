65a28877d3b47631fc8e0f94ac67cb5576c4ddbb
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-03-23 16:36:59 +0300

    Implement storage, nonce and class hash getters for JunoStateReader

--- src/juno_state_reader.rs
+++ src/juno_state_reader.rs
@@ -5,28 +5,120 @@ use starknet_rs::{
     utils::{Address, ClassHash},
 };
 use felt::Felt;
+use std:: { collections::HashMap, ffi::c_uchar, slice };
 
-pub struct JunoStateReader(pub usize); // uintptr_t equivalent
+extern {
+    fn JunoFree(ptr: *const c_uchar);
 
+    fn JunoStateGetStorageAt(reader_handle: usize, contract_address: *const c_uchar, storage_location: *const c_uchar) -> *const c_uchar;
+    fn JunoStateGetNonceAt(reader_handle: usize, contract_address: *const c_uchar) -> *const c_uchar;
+    fn JunoStateGetClassHashAt(reader_handle: usize, contract_address: *const c_uchar) -> *const c_uchar;
+}
+
+pub struct JunoStateReader{
+    pub handle: usize, // uintptr_t equivalent
+
+    address_to_storage: HashMap<StorageEntry, Felt>,
+    address_to_nonce: HashMap<Address, Felt>,
+    address_to_class_hash: HashMap<Address, ClassHash>,
+}
+
+impl JunoStateReader {
+    pub fn new(
+        handle: usize
+    ) -> Self {
+        Self {
+            handle: handle,
+            address_to_storage:  HashMap::new(),
+            address_to_nonce:  HashMap::new(),
+            address_to_class_hash: HashMap::new(),
+        }
+    }
+}
 
 impl StateReader for JunoStateReader {
-    fn get_contract_class(&mut self, class_hash: &ClassHash) -> Result<ContractClass, StateError> {
+    fn get_contract_class(&mut self, _class_hash: &ClassHash) -> Result<ContractClass, StateError> {
         todo!()
     }
 
     fn get_class_hash_at(&mut self, contract_address: &Address) -> Result<&ClassHash, StateError> {
-        todo!()
+        self.address_to_class_hash.remove(contract_address);
+
+
+        let addr = felt_to_byte_array(&contract_address.0);
+        let ptr = unsafe { JunoStateGetClassHashAt(self.handle, addr.as_ptr()) };
+        if !ptr.is_null() {
+            let felt_val = ptr_to_felt(ptr);
+            unsafe { JunoFree(ptr) };
+            self.address_to_class_hash.insert(contract_address.clone(), felt_to_byte_array(&felt_val));
+        }
+
+        let class_hash = self
+            .address_to_class_hash
+            .get(contract_address)
+            .ok_or_else(|| StateError::NoneClassHash(contract_address.clone()));
+        class_hash
     }
 
     fn get_nonce_at(&mut self, contract_address: &Address) -> Result<&Felt, StateError> {
-        todo!()
+        self.address_to_nonce.remove(contract_address);
+
+
+        let addr = felt_to_byte_array(&contract_address.0);
+        let ptr = unsafe { JunoStateGetNonceAt(self.handle, addr.as_ptr()) };
+        if !ptr.is_null() {
+            let felt_val = ptr_to_felt(ptr);
+            unsafe { JunoFree(ptr) };
+            self.address_to_nonce.insert(contract_address.clone(), felt_val);
+        }
+
+        let nonce = self
+            .address_to_nonce
+            .get(contract_address)
+            .ok_or_else(|| StateError::NoneContractState(contract_address.clone()));
+        nonce
     }
 
     fn get_storage_at(&mut self, storage_entry: &StorageEntry) -> Result<&Felt, StateError> {
-        todo!()
+        self.address_to_storage.remove(storage_entry);
+
+        let addr = felt_to_byte_array(&storage_entry.0.0);
+        let ptr = unsafe { JunoStateGetStorageAt(self.handle, addr.as_ptr(), storage_entry.1.as_ptr()) };
+        if !ptr.is_null() {
+            let felt_val = ptr_to_felt(ptr);
+            unsafe { JunoFree(ptr) };
+            self.address_to_storage.insert(storage_entry.clone(), felt_val);
+        }
+
+        let storage = self
+        .address_to_storage
+        .get(storage_entry)
+        .ok_or_else(|| StateError::NoneStorage(storage_entry.clone()));
+
+        storage
     }
 
     fn count_actual_storage_changes(&mut self) -> (usize, usize) {
         todo!()
     }
+}
+
+
+fn felt_to_byte_array(felt: &Felt) -> [u8; 32] {
+    let felt_bytes = felt.to_bytes_be();
+    let mut zeros = [0; 32];
+    let mut start_idx =  zeros.len() - felt_bytes.len();
+
+    for val in felt_bytes {
+        zeros[start_idx] = val;
+        start_idx += 1;
+    }
+
+    zeros
+}
+
+
+fn ptr_to_felt(bytes: *const c_uchar) -> Felt {
+    let slice = unsafe { slice::from_raw_parts(bytes, 32) };
+    Felt::from_bytes_be(slice)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,19 @@
 mod juno_state_reader;
 
+use starknet_rs::{
+    business_logic::state::{state_api::StateReader}, utils::Address,
+};
 use crate::juno_state_reader::JunoStateReader;
-
-extern {
-    fn JunoStateGetStorageAt(readerHandle: usize);
-}
+use felt::Felt;
 
 #[no_mangle]
-pub extern "C" fn cairoVMCall(readerHandle: usize) {
-    let reader = JunoStateReader(readerHandle);
+pub extern "C" fn cairoVMCall(reader_handle: usize) {
+    let mut reader = JunoStateReader::new(reader_handle);
     println!("called from Go");
-    unsafe { JunoStateGetStorageAt(reader.0); }
+
+    let addr = Address(Felt::new(44));
+    let entry = (Address(Felt::new(44)), [5; 32]);
+    println!("res {:?}", reader.get_storage_at(&entry));
+    println!("res {:?}", reader.get_nonce_at(&addr));
+    println!("res {:?}", reader.get_class_hash_at(&addr));
 }
